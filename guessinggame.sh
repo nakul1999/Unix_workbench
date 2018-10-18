9b4d3f5d83541074e7615de437a8b2e8b5d49967
nakul1999 <40247254+nakul1999@users.noreply.github.com>
2018-10-19 01:27:16 +0530

    Add files via upload

---
+++ guessinggame.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+let files_number=$( find . -maxdepth 1 -type f | wc -l )
+
+function compareAnswer {
+    if [[ $response -gt $files_number ]] ; then
+        echo "guess was high,try again..."
+        readAnswer
+    else
+        echo "guess was low,try again..."
+        readAnswer
+    fi
+}
+
+function readAnswer {
+    read response
+    while [[ ! $response =~ ^[0-9]+$ ]] ; do
+        echo "try again with a positive integer value"
+        read response
+    done
+}
+
+echo "guess how many files are in the current directory ($(pwd))..."
+readAnswer
+while [[ $response -ne $files_number ]] ; do
+    compareAnswer
+done
+echo "Congratulations, your answer is correct."
+
+exit 0
